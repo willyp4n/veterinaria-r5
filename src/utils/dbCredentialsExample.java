2095e896b3e9f5187833b635db78f5c187cd98fe
Jonathan Aldana <j4ckdev@gmail.com>
2021-08-31 12:07:46 -0500

    Instrucciones actualizadas

--- src/utils/dbCredentialsExample.java
+++ src/utils/dbCredentialsExample.java
@@ -2,8 +2,8 @@ package utils;
 
 /**
  * LÉEME
- * -> Cambie el nombre del archivo y de la clase por dbCredentials
- * -> Coloque los valores que corresponden para aceeder a su base de datos
+ * -> Crear un archivo con el nombre de dbCredentials y dentro crée una clase con el mismo nombre
+ * -> Copie las variables en el nuevo archivo y coloque los valores que corresponden para aceeder a su base de datos
  * @author j4ckdev
  */
 public class dbCredentialsExample {
