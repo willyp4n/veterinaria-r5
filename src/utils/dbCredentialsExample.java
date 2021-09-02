2ebe98339277693777548411c24cbbcc039e7f41
Jonathan Aldana <j4ckdev@gmail.com>
2021-09-02 00:30:28 -0500

    Correccion en dbCredentialsExample

--- src/utils/dbCredentialsExample.java
+++ src/utils/dbCredentialsExample.java
@@ -7,25 +7,8 @@ package utils;
  * @author j4ckdev
  */
 public class dbCredentialsExample {
-    private final String ip = "su ip de acceso"; // Si es un base de datos local, por defecto es 127.0.0.1
-    private final String accessPort = "su puerto"; // Por defecto es 3306
-    private final String user = "Su usuario de acceso a la base de datos"; 
-    private final String pass = "Su contraseña de acceso a la base de datos";
-
-    public String getIp() {
-        return ip;
-    }
-
-    public String getAccessPort() {
-        return accessPort;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-    
+    public final String ip = "su ip de acceso"; // Si es un base de datos local, por defecto es 127.0.0.1
+    public final String accessPort = "su puerto"; // Por defecto es 3306
+    public final String user = "Su usuario de acceso a la base de datos"; 
+    public final String pass = "Su contraseña de acceso a la base de datos";    
 }
